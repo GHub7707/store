e80da7043379f0bcfcf7fc69aa21c56661be3170
lom <shalinglom@sina.com>
2018-11-10 18:24:56 +0800

    default

---
+++ resources/views/Admin/Index/index.blade.php
@@ -0,0 +1,13 @@
+﻿@extends('admin.common.parent')
+
+@section('title', '后台首页')
+
+@section('style')
+@endsection
+
+@section('content')
+	<h1>这是后台首页</h1>
+@endsection
+
+@section('script')
+@endsection
