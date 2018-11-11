4e483f6e14ad3909e484823fc7430570d68c03bb
lom <shalinglom@sina.com>
2018-11-12 01:01:20 +0800

    default

---
+++ config/custom.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 50xiuer
+ * Date: 2018/11/11
+ * Time: 17:57
+ */
+
+return [
+    'pwd_salt'  => 'sdf676',
+];
--- config/database.php
+++ config/database.php
@@ -41,13 +41,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '203.195.132.143'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'store'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', '123456'),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
